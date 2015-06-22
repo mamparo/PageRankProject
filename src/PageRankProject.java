ba75be4e1654e306a172300f903b4dc8fbe5d897
mamparo <myronamparo@gmail.com>
2015-06-22 14:59:43 -0700

    Initalized Repo, Added print

---
+++ src/PageRankProject.java
@@ -0,0 +1,12 @@
+/**
+ * Created by myronamparo on 6/22/15.
+ */
+public class PageRankProject {
+
+    public static void main(String [] args){
+
+        System.out.print("Thank you lord gaben");
+
+    }
+
+}
