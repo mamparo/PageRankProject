5d0d3777afdd9feba0ba2e5475d0ca9bf08d0ee2
Goldu123 <goldu1@gmail.com>
2015-06-22 15:58:27 -0700

    Implementation of Database class and addition of the method getFile().

--- src/PageRankProject.java
+++ src/PageRankProject.java
@@ -1,14 +1,32 @@
+import java.io.File;
+
 /**
  * Created by myronamparo on 6/22/15.
  */
 public class PageRankProject {
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws Exception
+    {
+        //new PageRankProject
+        PageRankProject pRP = new PageRankProject();
 
-        System.out.print("Thank you lord gaben");
-        System.out.println("Yo");
-        //Goddammit Janet
-        System.out.print("POO");
+        //creates a new Database
+        Database d = new Database();
+        d.createDatabase();
+
+        System.out.println(pRP.getFile(1).getAbsolutePath());
     }
 
+    /**
+     * Returns the file that has the path that ends in the parameter number.
+     *
+     * @param number
+     * @return
+     */
+    public File getFile(int number)
+    {
+        String pathName = "src/files/doc" + String.valueOf(number) + ".txt";
+        File file = new File(pathName);
+        return file;
+    }
 }
