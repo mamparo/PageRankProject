5e469440eaac374ac9223cb14bf0cc3de2ccf038
Goldu123 <goldu1@gmail.com>
2015-06-23 15:33:23 -0700

    Percentages work.

--- src/PageRankProject.java
+++ src/PageRankProject.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 /**
  * Created by myronamparo on 6/22/15.
@@ -14,7 +18,59 @@ public class PageRankProject {
         Database d = new Database();
         d.createDatabase();
 
-        System.out.println(pRP.getFile(1).getAbsolutePath());
+        String[] keywords = pRP.getKeywordsFromUser();
+        File file = pRP.getFile(1);
+        DecimalFormat df = new DecimalFormat("#.####");
+        System.out.println(df.format(pRP.getPercentage(keywords, file)));
+    }
+
+    public String[] getKeywordsFromUser()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many keywords are you searching for? (Input as a number)");
+        while (!in.hasNextInt())
+        {
+            in.next();
+            System.out.println("Please enter a number.");
+        }
+        int numKeywords = in.nextInt();
+
+        String[] keywords = new String[numKeywords];
+        for (int i = 0; i < numKeywords; i++)
+        {
+            if (i == 0)
+                System.out.println("Enter a keyword.");
+            else
+                System.out.println("Enter another keyword.");
+            while (!in.hasNextLine())
+            {
+                in.next();
+                System.out.println("Please enter a valid word.");
+            }
+            keywords[i] = in.next();
+        }
+
+        return keywords;
+    }
+
+    public double getPercentage(String[] keywords, File file) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        int occurrences = 0;
+        String line;
+        int x = 0;
+        while ((line = br.readLine()) != null)
+        {
+            x++;
+            for (int i = 0; i < keywords.length; i++)
+            {
+                if (line.equals(keywords[i]))
+                    occurrences++;
+            }
+        }
+
+        return (((double)occurrences/x)*100);
     }
 
     /**
